ad7d0f32af495a51cd3009d750f130fe913514fe
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-04-06 14:37:38 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        { message: "What would you like to add in your todo?", name: "userFirstInput", type: "input" },
+        {
+            message: "What would you like to add more in your todo?",
+            name: "confirmInput",
+            type: "confirm",
+            default: "true"
+        },
+    ])
+    if (todoQuestions.userFirstInput == "") {
+        console.log("\n Please Add Your Todos \n")
+        condition = todoQuestions.confirmInput
+    } 
+    else {
+        todos.push(todoQuestions.userFirstInput)
+        condition = todoQuestions.confirmInput
+        todos.forEach((todo, index) => console.log(`${index}: ${todo}`))
+    }
+}
